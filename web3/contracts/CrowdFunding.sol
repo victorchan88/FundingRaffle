5950bf8530e1f7aa262780796c8675fd90fc5e5c
Victor Chan <victorchan654@gmail.com>
2023-09-24 03:24:06 -0400

    Converted smart contract to Base

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract CrowdFunding {
+import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+
+contract CrowdFunding is ERC721Base {
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps
+    )
+        ERC721Base(
+            _defaultAdmin,
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps
+        )
+    {}
+
     struct Campaign {
         address owner;
         string title;
